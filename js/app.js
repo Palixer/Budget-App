37f89101768f90b92aee5422313cae74e239eeb5
Gabriel Cuello <cgabrielcuello@gmail.com>
2021-12-26 23:04:05 -0300

    commit 3: new classes

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const ingresos = [
+    new Ingreso("salario", 2100.00),
+    new Ingreso("venta coche",1500)
+];
+
+const egresos = [
+    new Egreso("Renta departamento", 900),
+    new Egreso("Ropa", 400)
+];
+
+let cargarApp = () => {
+    cargarCabecero()
+}
